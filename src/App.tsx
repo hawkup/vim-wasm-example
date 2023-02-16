fbbbf3d714fce5350a79f91804bb98a0a6d460db
hawkup <spiritsquare@gmail.com>
2023-02-16 15:12:27 +0700

    add vim-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Vim } from 'react-vim-wasm';
+import workerURL from 'vim-wasm/vim.js?url'
 
+function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Vim
+        worker={workerURL}
+        readClipboard={navigator.clipboard && navigator.clipboard.readText}
+        onWriteClipboard={navigator.clipboard && navigator.clipboard.writeText}
+    />
   )
 }
 
